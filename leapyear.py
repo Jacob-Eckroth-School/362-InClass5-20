f54fe135bda7c13c5d68bd7433c0e5fb8af8904c
JacobEckroth <jacobeckroth@gmail.com>
2021-05-20 16:12:22 -0700

    removed error handling

--- leapyear.py
+++ leapyear.py
@@ -1,26 +1,24 @@
 def main():
    
-    valid = False
-    number = ""
-    while not valid:
-        number = input("Please enter the year you want to check for leapyear status:")
-        if(number.isnumeric()):
-            valid = True
-        if(not valid):
-            print("Invalid. Please enter a positive integer as the year.")
-
+   
+    number = input("Please enter the year you want to check for leapyear status:")
     intNumber = int(number)
 
-    if(intNumber % 400 == 0):
+    if(isLeapYear(intNumber)):
         print("The year " + number + " is a leap year!")
-    elif(intNumber %  100 == 0):
-        print("The year " + number + " is not a leap year.")
-    elif(intNumber % 4 == 0):
-        print("The year " + number  +" is a leap year!")
     else:
-        print("The year " + number + " is not a leap year." )
+        print("The year " + number + " is not a leap year.")
+    
 
-
+def isLeapYear(number):
+    if(number % 400 == 0):
+        return True
+    elif(number %  100 == 0):
+        return False
+    elif(number % 4 == 0):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
