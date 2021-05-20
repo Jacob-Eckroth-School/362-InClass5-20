765b97a692ec08052ba96bccadca91dfbfc1d905
JacobEckroth <jacobeckroth@gmail.com>
2021-05-20 16:52:10 -0700

    done

--- test_leapyear.py
+++ test_leapyear.py
@@ -25,3 +25,24 @@ class TestCase(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+#pytest code:
+def test_odd_year():
+        assert(not leapyear.isLeapYear(123))
+def test_odd_year_two():
+    assert( not leapyear.isLeapYear(1231232121))
+def test_divis_by_400():
+    assert(leapyear.isLeapYear(400))
+def test_divis_by_400_2():
+    assert(leapyear.isLeapYear(1200))
+def test_divis_by_100():
+    assert(not leapyear.isLeapYear(100))    #Shoudl be false, because divisible by 100
+def test_divis_by_300():
+    assert(not leapyear.isLeapYear(900))
+def test_divis_by_4():
+    assert(leapyear.isLeapYear(4))
+def test_divis_by_4():
+    assert(leapyear.isLeapYear(20))
+def test_divis_by_3():
+    assert(not leapyear.isLeapYear(9))
